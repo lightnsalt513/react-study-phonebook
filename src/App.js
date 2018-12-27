44e8009562473d6d4b575871de41bba52fe24fde
Jay-PC\Jay <jaydo@hivelab.co.kr>
2018-12-27 18:07:49 +0900

    Practicing react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
+import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    id = 2
+    state = {
+        information: [
+            {
+                id: 0,
+                name: '김민준',
+                phone: '010-0000-0000'
+            },
+            {
+                id: 1,
+                name: '홍길동',
+                phone: '010-0000-0001'
+            }
+        ]
+    }
+    
+    handleCreate = (data) => {
+        const { information } = this.state;
+        this.setState({
+            information: information.concat({ id: this.id++, ...data })
+        });
+    }
+    
+    handleRemove = (id) => {
+        const { information } = this.state;
+        this.setState({
+            information: information.filter(info => info.id !== id)
+        });  
+    }
+    
+    render() {
+        const { information } = this.state;
+        return (
+            <div className="App">
+                <PhoneForm onCreate={this.handleCreate} />
+                <PhoneInfoList
+                    data={information}
+                    onRemove={this.handleRemove}
+                />
+                {JSON.stringify(information)}
+            </div>
+        );
+    }
 }
 
 export default App;
